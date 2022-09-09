665a8072fccb74ee2ff6233611b7025248263244
Aaron Langley <alangley345@gmail.com>
2022-09-09 16:20:00 -0400

    Added seperate methods for getting tasks and drawing tasks

--- src/ToDo.java
+++ src/ToDo.java
@@ -8,11 +8,11 @@ import java.util.*;
 public class ToDo {
 	
 	public static final String currentUser = System.getProperty("user.name");
-	public static final String url = "jdbc:sqlite:/home/" + currentUser + "/.local/java-todo/" + currentUser + "_" + "todos.db";
+	public static final String url = "jdbc:sqlite:/home/" + currentUser + "/.local/share/java-todo/" + currentUser + "_" + "todos.db";
 	
 	private static void createNewDB() {
-		
-		File appDirectory = new File("/home/" + currentUser + "/.local/java-todo");
+		//check for individuals tables and create it if it doesn't exist
+		File appDirectory = new File("/home/" + currentUser + "/.local/share/java-todo");
 		appDirectory.mkdir();
 		String sqlCreateTable = "CREATE TABLE IF NOT EXISTS items (\n"
 				+ "     id integer PRIMARY KEY,\n"
@@ -36,29 +36,97 @@ public class ToDo {
         }
 	}
 	
-	public static void main(String[] args) {
-		createNewDB();
+	private static ArrayList<String[]> getTasks() {
+		String getAllTasks = "SELECT title, content FROM items;";
+		ArrayList<String[]> resultsList = new ArrayList<>();
 		
+		try (Connection conn = DriverManager.getConnection(url)){
+			Statement selectAllItems = conn.createStatement();
+			ResultSet allItems = selectAllItems.executeQuery(getAllTasks);
+			    	
+			while(allItems.next()) {
+				String resultTitle   = allItems.getString("title");
+				String resultContent = allItems.getString("content");
+				String[] resultArray = new String[] {resultTitle, resultContent};
+				resultsList.add(resultArray);
+			}
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return resultsList;
+		
+	}
+	
+	private static void drawTasks(ArrayList<String[]> resultsList, Shell shell, ExpandBar expandBar) {
+		for(int i = 0; i < resultsList.size(); i++) {
+			String[] temp = resultsList.get(i);
+			Composite itemComposite = new Composite(expandBar, SWT.NONE);
+			GridLayout itemGrid = new GridLayout();
+			itemGrid.numColumns = 1;
+			itemComposite.setLayout(itemGrid);
+			    		
+			//content
+			Text contentText = new Text(itemComposite,SWT.CENTER);
+			contentText.setText(temp[1]);
+			GridData contentData =  new GridData(SWT.FILL, SWT.CENTER, true, false);
+			contentText.setLayoutData(contentData);
+			contentText.setSize(shell.getSize().x,50);
+			    		
+			//title
+			ExpandItem item = new ExpandItem (expandBar, SWT.FILL);
+			item.setText(temp[0]);
+			item.setExpanded(true);
+			item.setHeight(100);
+			item.setControl(itemComposite);	
+			    		
+			Button itemButton = new Button(itemComposite,SWT.PUSH);
+			itemButton.setText("Delete");
+			itemButton.addListener(SWT.Selection, new Listener()
+			{
+				public void handleEvent(Event event){
+					String searchString = contentText.getText();
+			    	System.out.println(searchString);
+			    	String sql = "DELETE FROM items WHERE content=?";
+			    	try (Connection conn = DriverManager.getConnection(url)){
+			    		PreparedStatement pstmt = conn.prepareStatement(sql);
+			    		pstmt.setString(1, searchString);
+			    		pstmt.executeUpdate();
+			    	}
+			    	catch (SQLException e) {
+	    			    System.out.println(e.getMessage());
+	    			}
+	    			drawApp();
+			    } 	    
+			});
+		}
+	}
+	
+	private static void drawApp() {
 		//variables for shell
 		String title        = "My To Dos";
 		int    width        = 400;
-		int    height       = 800;
-		
-		//shell & bar
+		int    height       = 600;
+				
+		//shell
 		Display display = new Display();
 		Shell shell     = new Shell(display);
 		shell.setText(title);
 		shell.setSize(width,height);
 		shell.setLayout(new GridLayout()); 
-		ExpandBar expandBar = new ExpandBar (shell, SWT.FILL);
 		
-		//composite for expand bar
-		Composite barComposite    = new Composite(expandBar, SWT.NONE);
-		GridLayout barLayout      = new GridLayout();
-		barLayout.marginLeft      = barLayout.marginTop=
-	    barLayout.marginRight     = barLayout.marginBottom=8;
-	    barLayout.verticalSpacing = 10;
-	    barComposite.setLayout(barLayout);
+		//composite for expandbar
+		Composite barComposite = new Composite(shell, SWT.FILL);
+		GridData barCompositeGridData   = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+		barCompositeGridData.widthHint  = (shell.getSize().x);
+		barComposite.setLayoutData(barCompositeGridData);
+		
+		//expandbar for tasks
+		ExpandBar expandBar    = new ExpandBar(shell, SWT.FILL);
+		GridData expandBarGridData   = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+		expandBarGridData.widthHint  = (shell.getSize().x);
+		expandBar.setLayoutData(expandBarGridData);
 		
 		//Add item to table
 		/*Button addItemButton = new Button(shell,SWT.PUSH);
@@ -72,64 +140,30 @@ public class ToDo {
 				String sql = "INSERT INTO items(title,content) VALUES(?,?)";
 
 			    try (Connection conn = DriverManager.getConnection(url)){
-			    	PreparedStatement pstmt = conn.prepareStatement(sql);
-			        pstmt.setString(1, title);
-			        pstmt.setString(2, content);
-			        pstmt.executeUpdate();
-			    } 
-			    catch (SQLException e) {
-			        System.out.println(e.getMessage());
-			    }
+					PreparedStatement pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, title);
+					pstmt.setString(2, content);
+					pstmt.executeUpdate();
+				} 
+				catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
 			}  	    
 		});
 		*/
 		
-		String getAllTasks = "SELECT title, content FROM items;";
-		try (Connection conn = DriverManager.getConnection(url)){
-			Statement selectAllItems = conn.createStatement();
-	    	ResultSet allItems = selectAllItems.executeQuery(getAllTasks);
-	    
-	    	ArrayList<String[]> resultsList = new ArrayList<>();
-	    	
-	    	while(allItems.next()) {
-	    		String resultTitle   = allItems.getString("title");
-	    		String resultContent = allItems.getString("content");
-	    		String[] resultArray = new String[] {resultTitle, resultContent};
-	    		resultsList.add(resultArray);
-	    	}
-	    
-	    	for(int i = 0; i < resultsList.size(); i++) {
-	    		String[] temp = resultsList.get(i);
-	    		Composite itemComposite = new Composite(expandBar, SWT.NONE);
-	    		GridData itemData =  new GridData(SWT.FILL, SWT.CENTER, true, false);
-	    		itemData.horizontalAlignment = SWT.FILL;
-	    		itemData.grabExcessHorizontalSpace = true;
-	    		itemComposite.setLayoutData(itemData);
-	    		
-	    		//content
-	    		Text contentText = new Text(itemComposite,SWT.FILL);
-	    		contentText.setText(temp[1]);
-	    		GridData contentData =  new GridData(SWT.FILL, SWT.CENTER, true, false);
-	    		contentData.horizontalAlignment = SWT.FILL;
-	    		contentData.grabExcessHorizontalSpace = true;
-	    		contentText.setLayoutData(contentData);
-
-	    		//title
-	    		ExpandItem item = new ExpandItem (expandBar, SWT.FILL);
-	    		item.setText(temp[0]);
-	    		item.setExpanded(true);
-	    		item.setHeight(itemComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
-	    		item.setControl(itemComposite);	
-	    	}
-	    } 
-	    catch (SQLException e) {
-	        System.out.println(e.getMessage());
-	    }
-				
-		shell.open();			
+		ArrayList<String[]> results = getTasks();
+		drawTasks(results, shell, expandBar);
+		
+	    shell.open();			
 		while(!shell.isDisposed())
 			if (!display.readAndDispatch())
 				display.sleep();
 		display.dispose();
+	}
+	
+	public static void main(String[] args) {
+		createNewDB();
+		drawApp();
 	}
 }
