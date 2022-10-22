668030a74a841fb718f8029ba0ee20d83fff9451
Aaron Langley <alangley345@gmail.com>
2022-10-22 11:14:03 -0400

    Added export to PDF function

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # java-todo
-Todo app using SQLite and Java SWT
+Todo app using SQLite backend and Java SWT frontend. 
+Added export functionality via PDF and the ability to email yourself a list of your tasks. 
 
 ## Setup Documentation
 
     sudo dnf install -y sqlite sqlite-devel sqlite-tcl sqlite-jdbc
 
-This project also makes use of the sqlite-jdbc driver from https://search.maven.org/artifact/org.xerial/sqlite-jdbc/3.39.2.0/jar.
+This project also makes use of the following external jars:
+* Java SWT from https://download.eclipse.org/eclipse/downloads/index.html#Latest_Release
+* sqlite-jdbc libery from https://search.maven.org/artifact/org.xerial/sqlite-jdbc/3.39.2.0/jar.
+* Apache PDFbox library from https://www.apache.org/dyn/closer.lua/pdfbox/2.0.27/pdfbox-app-2.0.27.jar 
 
-I grabbed the stand alone jar rather than use a build tool like Maven, but that may change later for consistency. 
+I may turn this into a Maven or Gradle project later. 
 
 Useful for testing:
 
--- src/com/ToDo/ToDo.java
+++ src/com/ToDo/ToDo.java
@@ -217,36 +217,43 @@ public class ToDo {
 	
 	private static void savePDF() {
 		ArrayList<String[]> resultsList = getTasks();
-		PDDocument document = new PDDocument();
 		int offset = 700;
 		
 		try {
+			PDDocument document = new PDDocument();
 			PDPage newPage = new PDPage();
 			document.addPage(newPage);
 			PDPage tasks = document.getPage(0);
 			PDPageContentStream contentStream = new PDPageContentStream(document, tasks);
 			contentStream.setFont(PDType1Font.COURIER, 16);
-			contentStream.beginText();
-			contentStream.newLineAtOffset(25, offset);
+			contentStream.setLeading(1.15f);
 			
+			//header
+			contentStream.beginText();
+			contentStream.newLineAtOffset(200, 750);
+			contentStream.showText(currentUser + "'s To-Dos");
+			contentStream.newLineAtOffset(0, -15);
+			contentStream.newLineAtOffset(0, -15);
+			contentStream.endText();
+			
+			//tasks
+			contentStream.beginText();
+			contentStream.newLineAtOffset(25, 700);
 			for(int i = 0; i < resultsList.size(); i++) {
 				String[] temp = resultsList.get(i);
-				contentStream.showText(temp[0]);
-				contentStream.newLine();
-				offset += 100;
+				contentStream.newLineAtOffset(0, -15);
+				contentStream.showText(i+1 + ". " + temp[0]);
 			}
 			
 			contentStream.endText();
 			contentStream.close();
 			document.save("/home/aaron/Downloads/pdf_test.pdf");
 			document.close();
-		} catch (IOException e1) {
+		}
+		catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		
-		
 	}
 
 	public static void drawGUI() {
